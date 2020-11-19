d8057a8f7e3763e560919bec1e6d9731c1d6a488
86180 <473999190@qq.com>
2020-11-19 21:43:17 +0800

    第三版

--- config.py
+++ config.py
@@ -6,6 +6,4 @@ password = "123456"
 DB = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(username,password,host,port,database)
 SQLALCHEMY_DATABASE_URI = DB
 
-#设置数据库追踪信息,压制警告
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-
