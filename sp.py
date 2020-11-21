67a41739516fb4bb82f0e9203ba727da0d07c4a4
86180 <473999190@qq.com>
2020-11-21 11:39:57 +0800

    第四版本

---
+++ sp.py
@@ -0,0 +1,14 @@
+# -- * -- coding : utf-8 -- * --
+# @Author	    	:	Mir.xie
+# @Project	    	:	weather
+# @File		    	:	sp.py
+# @Time		    	:	2020/11/21 11:16
+# @Contact	    	:	473999190qq.com
+# @License	    	:	(C) Copyright 2019-2020
+# @Software(IDE)	:	PyCharm
+# @Site		    	:	https://www.jetbrains.com/pycharm/
+# @Version	    	:	Python 3.8.0
+
+
+if __name__ == '__main__':
+    pass
